6d7bb8ce5effb7b17b60a2d655d6a8942187ba7f
macluiggy <ferrinluiggy@gmail.com>
2021-10-26 11:45:26 -0500

    item 1 finished

--- chapter1.js
+++ chapter1.js
@@ -1,11 +1,10 @@
 // Item 1
 // Se recomienda tener activada la opcion noImplicitAny: true para que typescript detecte los errores en los typos de variables
 // es apropiado dejarlo desactivado cuando se esta transicionando de un projecto a otro
-function add(a: number, b: number) {
-    return a + b
+function add(a, b) {
+    return a + b;
 }
-add(10, 23)
-
+add(10, 23);
 // strictNullChecks controla si null y undefined son valores permisibles en cada typo
-const x: number | null = null //no se muestra error si strictNullChecks esta desactivado
+var x = null; //no se muestra error si strictNullChecks esta desactivado
 // al igual que noImplicitAny, strictNullChecks no se activa si se esta emigrando en un projecto
