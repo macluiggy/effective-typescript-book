64e1dafade54e195aabc889a141d175544176913
macluiggy <ferrinluiggy@gmail.com>
2021-10-26 15:16:43 -0500

    test

--- chapter1.js
+++ chapter1.js
@@ -5,6 +5,19 @@ function add(a, b) {
     return a + b;
 }
 add(10, 23);
-// strictNullChecks controla si null y undefined son valores permisibles en cada typo
+// strictNullChecks controla si null y undefined son valores permisibles en cada typon
 var x = null; //no se muestra error si strictNullChecks esta desactivado
 // al igual que noImplicitAny, strictNullChecks no se activa si se esta emigrando en un projecto
+//Item 3: Understand That Code Generation Is Independent of Types
+var saluda = 'hello';
+saluda = 1234; //typescript gives you a message error, that the original variable is a string, not a number
+function calculateShape(shape) {
+    if ('height' in shape) {
+        return shape.width * shape.height;
+    }
+    else {
+        return Math.pow(shape.width, 2);
+    }
+}
+console.log(calculateShape({ width: 12, height: 12 }));
+console.log(calculateShape({ width: 12 }));
--- chapter1.ts
+++ chapter1.ts
@@ -6,12 +6,33 @@ function add(a: number, b: number) {
 }
 add(10, 23)
 
-// strictNullChecks controla si null y undefined son valores permisibles en cada typo
+// strictNullChecks controla si null y undefined son valores permisibles en cada typon
 const x: number | null = null //no se muestra error si strictNullChecks esta desactivado
 // al igual que noImplicitAny, strictNullChecks no se activa si se esta emigrando en un projecto
-//
-// hola ahora estoy cambiando esta linea jaja
-//
-// nueva linea de codigo
-// ja
-// ja
+
+//Item 3: Understand That Code Generation Is Independent of Types
+
+let saluda = 'hello'
+saluda = 1234 //typescript gives you a message error, that the original variable is a string, not a number
+//but it won't prevent you from the build, they are just warnings of potencial error
+//You cannot check typescript types at runtime
+interface Square {
+    width: number;
+}
+interface Rectangle extends Square {
+    height: number
+}
+//depending of what argument we give it shape will be defined, for example, if we give it a variable 
+// x = { width: 12 } it would be defined as Square, since it doesn't have the variable doesnt have the height
+// property, but if we give it a variable y = { width: 13, height: 14 } it would be defined as Rectangle, since
+// this variable have the height property
+type Shape = Square | Rectangle;
+function calculateShape(shape: Shape) {
+    if ('height' in shape) {
+        return shape.width * shape.height
+    } else {
+        return shape.width ** 2
+    }
+}
+console.log(calculateShape({width: 12, height: 12}))
+console.log(calculateShape({width: 12})) //
